b27b763fa6a7a79ae32cfad47c3c8f55985005fc
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-19 15:34:10 -0500

    working on searchField component, able to update the state of a searchfield input

--- main/src/App.js
+++ main/src/App.js
@@ -62,6 +62,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <input type="search" placeholder="Search Monsters"/>
         <CardList 
           monstersData={ this.state.monsters }
         /> 
