aa71b6966c091053f01d6ec45c1a037fb25fae7e
ringosimonchen0820 <ringosimonchen0820@gmail.com>
2020-02-26 02:42:54 -0500

    Updates

---
+++ precache-manifest.bfeb795ebbd3e78839d98000ef43b3a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889690ceaf364f32559cc1cf081ff525",
+    "url": "/-ReactDevProject---MonstersRolodex/index.html"
+  },
+  {
+    "revision": "ee4ec7538cb1c1a87d6a",
+    "url": "/-ReactDevProject---MonstersRolodex/static/css/main.eef7fd5c.chunk.css"
+  },
+  {
+    "revision": "a5e59b2f591ee21ef91b",
+    "url": "/-ReactDevProject---MonstersRolodex/static/js/2.882d4128.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/-ReactDevProject---MonstersRolodex/static/js/2.882d4128.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee4ec7538cb1c1a87d6a",
+    "url": "/-ReactDevProject---MonstersRolodex/static/js/main.62f3a8c8.chunk.js"
+  },
+  {
+    "revision": "af01645b43202899ea9b",
+    "url": "/-ReactDevProject---MonstersRolodex/static/js/runtime-main.5df2c0eb.js"
+  }
+]);
